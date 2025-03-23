8c5a1ab62c0a2800e07eab0b4af8b3d7adf56eb1
CannonWest <cannon.west@alliance-it.com>
2025-03-23 17:27:35 -0400

    removed old viewmodel

--- src/viewmodels/__init__.py
+++ src/viewmodels/__init__.py
@@ -2,6 +2,5 @@
 ViewModels package for the OpenAI Chat application.
 """
 
-from src.viewmodels.conversation_viewmodel import ConversationViewModel
 from src.viewmodels.reactive_conversation_viewmodel import ReactiveConversationViewModel
 from src.viewmodels.settings_viewmodel import SettingsViewModel
